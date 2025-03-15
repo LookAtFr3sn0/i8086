872b5896625e248363f8f2eb073b9d65a9997809
LookAtFr3sn0 <lookatfr3sn0@gmail.com>
2025-03-15 07:13:14 +0100

    Add main module to initialize and run emulator

---
+++ main.py
@@ -0,0 +1,15 @@
+from cpu import CPU
+
+class Main:
+    def __init__(self):
+        self.cpu = CPU()
+       
+    def run(self):
+        pass
+
+    def run_once(self):
+        pass
+
+if __name__ == "__main__":
+    Emulator = Main()
+    Emulator.run()
